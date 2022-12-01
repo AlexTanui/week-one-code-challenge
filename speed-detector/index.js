a335f7b91453edeac2d91a9810b0c0ea5db3394b
AlexTanui <tanuialex417@gmail.com>
2022-12-01 12:40:00 +0300

    completed assignment

--- speed-detector/index.js
+++ speed-detector/index.js
@@ -6,11 +6,15 @@ function checkSpeed() {
   let speed = parseInt(document.getElementById("input").value)
     if (speed <= speedLimit + kmPerPoint) {
         document.getElementById("message").innerHTML = 'OK'
+        
+        
         return
     }
 //     for unrealistic speed on our roads 
      else if (speed > 200){
          document.getElementById("message").innerHTML = 'use a realistic speed!'
+
+         return
     }
 //  used math.random and math.floor to return a pseudorandom double type number greater than or equal to a number without a decimal
     const points = Math.floor((speed - speedLimit) / kmPerPoint);
