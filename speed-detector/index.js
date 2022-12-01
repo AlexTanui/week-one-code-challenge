9019c03b319fd8831bff323979584ddb6f511169
Alex <50584983+AlexTanui@users.noreply.github.com>
2022-12-01 12:07:05 +0300

    Update index.js

--- speed-detector/index.js
+++ speed-detector/index.js
@@ -1,3 +1,5 @@
+
+// funtion to check speed limit
 function checkSpeed() {
     const speedLimit = 70;
     const kmPerPoint = 5;
@@ -7,7 +9,7 @@ function checkSpeed() {
         return
      
     }
-  
+//  used math.random and math.floor to return a pseudorandom double type number greater than or equal to a number without a decimal
     const points = Math.floor((speed - speedLimit) / kmPerPoint);
     if (points >= 12) 
     document.getElementById("message").innerHTML =("License suspended" + " " + "by" + " " +  points + " " + "points");
